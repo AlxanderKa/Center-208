11ad7ca36b3f334e2a59c2025823704219487d16
Alex <sanyakarelin1@gmail.com>
2023-06-01 13:05:57 +0300

    Add class Point

--- src/main/java/work/Main.java
+++ src/main/java/work/Main.java
@@ -1,7 +1,45 @@
 package work;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    //Scanning data from a file and creating a set of trajectory points
+    public static List<Point> addFromFile(String path){
+        ArrayList<Point> points = new ArrayList<>();
+       // Path path = Path.of(s);
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    new FileInputStream(path), "UTF-8"));
+            try {
+                while (true) {
+                    String line = reader.readLine();
+                    if (line == null) {
+                        break;
+                    }
+                    String[] dotdata = line.split(" ");
+                    Point dot = new Point(Double.parseDouble(dotdata[0]), Double.parseDouble(dotdata[1]),
+                            Double.parseDouble(dotdata[2]), Double.parseDouble(dotdata[3]),
+                            Double.parseDouble(dotdata[4]), Double.parseDouble(dotdata[5]),
+                            Double.parseDouble(dotdata[6]));
+                    points.add(dot);
+
+                }
+
+            } finally {
+                reader.close();
+            }
+        } catch (IOException e) {
+            System.out.println("no file in/out");
+        }
+        return points;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
--- src/main/java/work/Point.java
+++ src/main/java/work/Point.java
@@ -1,4 +1,28 @@
 package work;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class Point {
+    private double t, B, L, H, Vx, Vy, Vz;
+
+    public Point(double t, double B, double L, double H, double Vx, double Vy, double Vz){
+        this.t=t;
+        this.B=B;
+        this.L=L;
+        this.H=H;
+        this.Vx=Vx;
+        this.Vy=Vy;
+        this.Vz=Vz;
+    }
+    public double getSpeed (Point p){
+        double speed = Math.sqrt(p.getVx()*p.getVx()*+p.getVy()*p.getVy()+p.getVz()*p.getVz());
+        return speed;
+    }
+
+
 }
