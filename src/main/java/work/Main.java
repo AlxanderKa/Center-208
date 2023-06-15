7b255f92589e0407ef06c9d077f1a104f5c53efa
Alex <sanyakarelin1@gmail.com>
2023-06-15 15:31:38 +0300

    Add Graphics and update UI

--- src/main/java/work/Main.java
+++ src/main/java/work/Main.java
@@ -5,7 +5,12 @@ import java.io.*;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
 
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.category.DefaultCategoryDataset;
 
 public class Main {
     //Scanning data from a file and creating a set of trajectory points
@@ -41,9 +46,35 @@ public class Main {
         }
         return points;
     }
+    public static XYDataset createDataset(final int idx, List<Point> points)//Creating DataSet for Graphics
+    {
+
+
+        final XYSeries series1 = new XYSeries("Vx");
+        final XYSeries series2 = new XYSeries("Vy");
+        final XYSeries series3 = new XYSeries("Vz");
+        for (int i = 0; i < points.size(); i++) {
+            series1.add(points.get(i).getT(), points.get(i).getVx());
+            series2.add(points.get(i).getT(), points.get(i).getVy());
+            series3.add(points.get(i).getT(), points.get(i).getVz());
+        }
+
+        final XYSeriesCollection dataset = new XYSeriesCollection();
+        if (idx == -1) {
+            dataset.addSeries(series1);
+            dataset.addSeries(series2);
+            dataset.addSeries(series3);
+        } else if (idx == 0) {
+            dataset.addSeries(series1);
+        } else if (idx == 1) {
+            dataset.addSeries(series2);
+        } else if (idx == 2) {
+            dataset.addSeries(series3);
+        }
+        return dataset;
+    }
 
     public static void main(String[] args) {
-
 
     }
 }
--- src/main/java/work/TestUI.java
+++ src/main/java/work/TestUI.java
@@ -1,5 +1,16 @@
 package work;
 
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.axis.ValueAxis;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYSplineRenderer;
+import org.jfree.chart.ui.RectangleInsets;
+import org.jfree.data.xy.XYDataset;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -13,8 +24,81 @@ import static work.Main.addFromFile;
 
 public class TestUI extends JFrame {
 
+    private JFreeChart createChart(List<Point> points) {
+        final JFreeChart chart = ChartFactory.createXYLineChart(
+                "Графики",
+                null,                        // x axis label
+                null,                        // y axis label
+                null,                        // data
+                PlotOrientation.VERTICAL,
+                true,                        // include legend
+                true,                       // tooltips
+                false                        // urls
+        );
+
+        chart.setBackgroundPaint(Color.white);
+
+        final XYPlot plot = chart.getXYPlot();
+        plot.setBackgroundPaint(new Color(232, 232, 232));
+
+        plot.setDomainGridlinePaint(Color.gray);
+        plot.setRangeGridlinePaint(Color.gray);
+
+        // Определение отступа меток делений
+        plot.setAxisOffset(new RectangleInsets(1.0, 1.0, 1.0, 1.0));
+
+        // Скрытие осевых линий и меток делений
+        ValueAxis axis = plot.getDomainAxis();
+        axis.setAxisLineVisible(true);    // осевая линия
+
+        // Настройка NumberAxis
+        final NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
+        rangeAxis.setAxisLineVisible(true);
+        rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+
+        // Настройка XYSplineRenderer
+        // Precision: the number of line segments between 2 points [default: 5]
+        XYSplineRenderer r0 = new XYSplineRenderer(8);
+        r0.setSeriesShapesVisible(0, true);
+
+        XYSplineRenderer r1 = new XYSplineRenderer();
+        r1.setPrecision(8);
+        r1.setSeriesShapesVisible(0, true);
+
+        XYSplineRenderer r2 = new XYSplineRenderer(8);
+
+        r2.setSeriesShapesVisible(0, true);
+        r2.setSeriesPaint(0, Color.orange);
+        r2.setSeriesStroke(0, new BasicStroke(2.5f));
+
+        // Наборы данных
+        XYDataset dataset0 = Main.createDataset(0, points);
+        XYDataset dataset1 = Main.createDataset(1, points);
+        XYDataset dataset2 = Main.createDataset(2, points);
+
+        plot.setDataset(0, dataset0);
+        plot.setDataset(1, dataset1);
+        plot.setDataset(2, dataset2);
+
+        // Подключение Spline Renderer к наборам данных
+        plot.setRenderer(0, r0);
+        plot.setRenderer(1, r1);
+        plot.setRenderer(2, r2);
+
+
+        return chart;
+    }
+
     public TestUI() {
         Box contents = new Box(BoxLayout.Y_AXIS);
+        final JSplitPane splitHorizontal = new JSplitPane();
+        splitHorizontal.setDividerSize(8);
+        splitHorizontal.setDividerLocation(150);
+        JSplitPane splitVerticalL = new JSplitPane(JSplitPane.VERTICAL_SPLIT, true);
+        JSplitPane splitVerticalR = new JSplitPane(JSplitPane.VERTICAL_SPLIT, true);
+        splitVerticalL.setDividerLocation(100);
+        splitVerticalR.setDividerLocation(100);
+
         JButton start = new JButton();
         start.setText("Start");
         contents.add(start);
@@ -34,14 +118,27 @@ public class TestUI extends JFrame {
                     JTextArea text = new JTextArea(10, 100);
 
                     for (int i = 0; i < points.size(); i++) {
-                        text.append(points.get(i).toString()+"\n");
-                      //  text.setLineWrap(true);
-                        outPoints[i] = points.get(i).toArray();
+                        text.append(points.get(i).toString() + "\n");//add string to area
+                        //  text.setLineWrap(true);
+                        outPoints[i] = points.get(i).toArray();//add data on table
 
                     }
-                    JTable table1 = new JTable(outPoints, points.get(0).getName());
-                    contents.add(new JScrollPane(table1));
-                    contents.add(new JScrollPane(text));
+                    JFreeChart chart = createChart(points);
+                    ChartPanel chartPanel = new ChartPanel(chart);
+
+                    chartPanel.setPreferredSize(new java.awt.Dimension(560, 480));
+
+                    JTable table1 = new JTable(outPoints, points.get(0).getName());//create data table
+                    splitVerticalL.setTopComponent(new JScrollPane());
+                    splitVerticalL.setBottomComponent(new JScrollPane(text));
+                    splitVerticalR.setTopComponent(new JScrollPane(table1));
+                    splitVerticalR.setBottomComponent(new JScrollPane(chartPanel));
+                    splitHorizontal.setLeftComponent(splitVerticalL);
+                    splitHorizontal.setRightComponent(splitVerticalR);
+                    contents.add(splitHorizontal);
+                    //  contents.add(chartPanel);
+                    //  contents.add(new JScrollPane(table1));
+                    // contents.add(new JScrollPane(text));
                 }
             }
 
